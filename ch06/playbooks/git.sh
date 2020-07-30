c3560323a61fc089b9f59bf72f2fa71a4a3ecff2
William Munoz Rodas <william@Williams-iMac.local>
2020-07-29 22:03:42 -0500

    Updating the mezzanine deployment on Chapter 06

--- ch06/playbooks/git.sh
+++ ch06/playbooks/git.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ansible web -m git -a "repo=git@github.com:lorin/mezzanine-example.git dest=/home/vagrant/mezzanine-example/project accept_hostkey=yes"
+ansible web -m git -a "repo=https://github.com/lorin/mezzanine-example.git dest=/home/william/mezzanine-example/project accept_hostkey=yes"
